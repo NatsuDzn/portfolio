dabd27fd56657e8cdcfaa65c9a39b7e18b29d5eb
NatsuDzn <natsuxgraph@gmail.com>
2022-11-08 18:57:38 +0100

    🧹 chore(next.config.js): add missing images domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["dl.airtable.com"],
+    dangerouslyAllowSVG: true,
+    domains: ["dl.airtable.com", "v5.airtableusercontent.com"],
   },
 };
