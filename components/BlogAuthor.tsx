ef5f01c8448c858f832c87c2e29270a3a26ffe02
NatsuDzn <natsuxgraph@gmail.com>
2023-04-30 20:27:35 +0200

    ♻️ refactor(blogauthor): use Flex component

--- components/BlogAuthor.tsx
+++ components/BlogAuthor.tsx
@@ -1,8 +1,7 @@
 import {
   Avatar,
-  HStack,
+  Flex,
   IconButton,
-  Stack,
   Text,
   Tooltip,
   useClipboard,
@@ -15,32 +14,35 @@ const BlogAuthor = ({ readingTime, publishedAt, url, ...props }) => {
   const { hasCopied, onCopy } = useClipboard(url);
 
   return (
-    <Stack direction="row" justify="space-between" {...props}>
-      <HStack>
-        <Avatar name="Nathanael Louzoun" src="/assets/images/me.jpg" h={10} w={10}></Avatar>
+    <Flex direction="row" justify="space-between" {...props}>
+      <Flex alignItems="center">
+        <Avatar
+          name="Nathanael Louzoun"
+          src="/assets/images/me.jpg"
+          h={10}
+          w={10}
+        ></Avatar>
         <VStack spacing={0} align="start">
           <Text fontSize="md" fontWeight="medium">
             Nathanael Louzoun
           </Text>
           <Text fontSize="xs" fontWeight="normal">
-            {format(publishedAt)} / {readingTime} 
+            {format(publishedAt)} / {readingTime}
           </Text>
         </VStack>
-      </HStack>
-      <HStack>
-        <Tooltip label={hasCopied ? "Copied !" : "Copy link"} placement="top">
-          <IconButton
-            onClick={onCopy}
-            aria-label="Copy link"
-            ml={2}
-            variant="outline"
-            size="sm"
-            color={hasCopied ? "green.400" : null}
-            icon={hasCopied ? <FaCheck /> : <FaLink />}
-          ></IconButton>
-        </Tooltip>
-      </HStack>
-    </Stack>
+      </Flex>
+      <Tooltip label={hasCopied ? "Copied !" : "Copy link"} placement="top">
+        <IconButton
+          onClick={onCopy}
+          aria-label="Copy link"
+          ml={2}
+          variant="outline"
+          size="sm"
+          color={hasCopied ? "green.400" : null}
+          icon={hasCopied ? <FaCheck /> : <FaLink />}
+        ></IconButton>
+      </Tooltip>
+    </Flex>
   );
 };
 
